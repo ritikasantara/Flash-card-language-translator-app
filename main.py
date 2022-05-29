42d179edd417275176a6d3c0c5988c2caca590b7
ritikasantara <rits.santara@gmail.com>
2022-05-29 20:24:49 +0530

    removing words from the current word list if tick is button is pressed

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 from tkinter import *
 import pandas
 import random
+import time
+
 BACKGROUND_COLOR = "#B1DDC6"
+current_data = {}
+words_to_learn = {}
+
+try:
+    data = pandas.read_csv("to_learn.csv")
+except:
+    data = pandas.read_csv("french_words.csv")
+    current_data = data.to_dict(orient="records")
+else:
+    current_data = data.to_dict(orient="records")
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+
 # methods
 
 
 def next_card():
-    with open("french_words.csv", mode="r") as file:
-        data = pandas.read_csv(file)
-        data = data.to_dict(orient="records")
-        current_data = random.choice(data)
-        canvas.itemconfig(card_title, text="French")
-        canvas.itemconfig(card_word, text=f"{current_data['French']}")
-        
+    global current_data
+    global words_to_learn
+    global flip_timer
+    window.after_cancel(flip_timer)
+    words_to_learn = random.choice(current_data)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=f"{words_to_learn['French']}", fill="black")
+    canvas.itemconfig(canvas_image, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=f"{words_to_learn['English']}", fill="white")
+    canvas.itemconfig(canvas_image, image=card_back_image)
+
+
+def is_known():
+    current_data.remove(words_to_learn)
+    to_learn = pandas.DataFrame(current_data)
+    to_learn.to_csv("to_learn.csv")
+
+    next_card()
+
+
+flip_timer = window.after(3000, func=flip_card)
 
 # canvas
 
 
 card_front_image = PhotoImage(file="card_front.png")
+card_back_image = PhotoImage(file="card_back.png")
 canvas = Canvas(width=800, height=536)
-canvas.create_image(400, 263, image=card_front_image)
+canvas_image = canvas.create_image(400, 263, image=card_front_image)
 canvas.config(highlightthickness=0, bg=BACKGROUND_COLOR)
 card_title = canvas.create_text(400, 150, text="", font=("Arial", 48, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Arial", 68, "bold"))
@@ -36,9 +71,8 @@ cross_image = PhotoImage(file="wrong.png")
 cross_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 cross_button.grid(column=0, row=1)
 
-
 tick_image = PhotoImage(file="right.png")
-tick_button = Button(image=tick_image, highlightthickness=0, command=next_card)
+tick_button = Button(image=tick_image, highlightthickness=0, command=is_known)
 tick_button.grid(column=1, row=1)
 
 next_card()
